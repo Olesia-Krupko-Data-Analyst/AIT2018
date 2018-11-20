f998cf7cff9cb2ea634077000ad1f9358fdb80fe
Олеся Крупко <okrupko@MacBook-Air-Olesa.local>
2018-11-20 22:16:56 +0300

    changetoken

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class BotHandler:
                 last_update = get_result[len(get_result)]
             return last_update
 
-greet_bot = BotHandler('536905508:AAGYnuJO7R1smQ0mU4yW-0Eh0FDW7bQMJTI')
+greet_bot = BotHandler(bot,'536905508:AAGYnuJO7R1smQ0mU4yW-0Eh0FDW7bQMJTI')
 greetings = ('hello', 'hi', 'greetings', 'sup')
 now = datetime.datetime.now()
 def main():
@@ -32,11 +32,11 @@ def main():
     hour = now.hour
     while True:
         greet_bot.get_updates(new_offset)
-    last_update = greet_bot.get_last_update()
-    last_update_id = last_update['update_id']
-    last_chat_text = last_update['message']['text']
-    last_chat_id = last_update['message']['chat']['id']
-    last_chat_name = last_update['message']['chat']['first_name']
+        last_update = greet_bot.get_last_update()
+        last_update_id = last_update['update_id']
+        last_chat_text = last_update['message']['text']
+        last_chat_id = last_update['message']['chat']['id']
+        last_chat_name = last_update['message']['chat']['first_name']
     if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
             greet_bot.send_message(last_chat_id, 'Good Morning  {}'.format(last_chat_name))
             today += 1
