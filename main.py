749655248644ea490322ff5e9306e5f019581667
Олеся Крупко <alesiakrupko@yandex.ru>
2018-11-21 20:44:24 +0300

    almost ok

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ class BotHandler:
             resp = requests.post(self.api_url + method, params)
             return resp
     def get_last_update(self):
-            get_result = self.get_updates()
-    if len(get_result) > 0:
-        last_update = get_result[-1]
-    else:
-        last_update = get_result[len(get_result)]
-return last_update
+        get_result = self.get_updates()
+        if len(get_result) > 0:
+            last_update = get_result[-1]
+        else:
+            last_update = get_result[len(get_result)]
+        return last_update
 
 greet_bot = BotHandler('751365230:AAGAFrTWzYNX1_Qm2D4v6hOoM1dkF7dqP50')
 greetings = ('hello', 'hi', 'greetings', 'sup')
@@ -30,14 +30,13 @@ def main():
     new_offset = None
     today = now.day
     hour = now.hour
-
-while True:
-    greet_bot.get_updates(new_offset)
+    while True:
+        greet_bot.get_updates(new_offset)
 last_update = greet_bot.get_last_update()
-    last_update_id = last_update['update_id']
-    last_chat_text = last_update['message']['text']
-    last_chat_id = last_update['message']['chat']['id']
-    last_chat_name = last_update['message']['chat']['first_name']
+last_update_id = last_update['update_id']
+last_chat_text = last_update['message']['text']
+last_chat_id = last_update['message']['chat']['id']
+last_chat_name = last_update['message']['chat']['first_name']
 if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
     greet_bot.send_message(last_chat_id, 'Good Morning  {}'.format(last_chat_name))
     today += 1
