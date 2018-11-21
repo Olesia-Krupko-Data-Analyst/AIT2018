c0b0a751f490b8ff0b48599f10542208eb9f86ba
Олеся Крупко <alesiakrupko@yandex.ru>
2018-11-21 22:36:36 +0300

    dffdfdf

--- main.py
+++ main.py
@@ -45,15 +45,15 @@ def main():
         last_chat_name = last_update['message']['chat']['first_name']
 
         if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-            greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
+            greet_bot.send_message(last_chat_id, 'Доброе утро, {}, кстати Владос-кокос приглашает вас в гости обращаться по номеру +7999999999'.format(last_chat_name))
             today += 1
 
         elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-            greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
+            greet_bot.send_message(last_chat_id, 'Добрый день, {}, кстати Владос-кокос приглашает вас в гости обращаться по номеру +7999999999'.format(last_chat_name))
             today += 1
 
         elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-            greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
+            greet_bot.send_message(last_chat_id, 'Добрый вечер, {}, кстати Владос-кокос приглашает вас в гости обращаться по номеру +7999999999'.format(last_chat_name))
             today += 1
 
         new_offset = last_update_id + 1
